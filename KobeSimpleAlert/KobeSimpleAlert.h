14299117270de6c7aad5fe48fca3b2ba1f656158
Kobe <mskang@xingxingmobility.com>
2023-07-10 12:54:33 +0900

    feat: For cartage framework

---
+++ KobeSimpleAlert/KobeSimpleAlert.h
@@ -0,0 +1,18 @@
+//
+//  KobeSimpleAlert.h
+//  KobeSimpleAlert
+//
+//  Created by Minseong Kang on 2023/07/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KobeSimpleAlert.
+FOUNDATION_EXPORT double KobeSimpleAlertVersionNumber;
+
+//! Project version string for KobeSimpleAlert.
+FOUNDATION_EXPORT const unsigned char KobeSimpleAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KobeSimpleAlert/PublicHeader.h>
+
+
